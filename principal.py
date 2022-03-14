fe74e8d370913803ed863bb9a4c1ad3e8fb7cb27
gabrielchanchig <101540243+gabrielchanchig@users.noreply.github.com>
2022-03-14 10:38:52 -0500

    Update principal.py
    Se incluyó el método para realizar el cálculo del perímetro

--- principal.py
+++ principal.py
@@ -7,8 +7,11 @@ def calc_area(lado):
 def calc_area_circulo(radio):
 	area=math.pi*radio**2
 	print(f"El área es {area}")
-	
+
+def calcular_perimetro(radio):
+	perim=2*math.pi*radio
+	print(f"El perímetro es {perim}")
 	
 calc_area(2)
 calc_area_circulo(3)
-	
+calcular_perimetro(4)	
