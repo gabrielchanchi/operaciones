c63f49f02379321f558b67d967d25655bb93f06b
gabrielchanchig <101540243+gabrielchanchig@users.noreply.github.com>
2022-03-13 23:05:47 -0500

    Update principal.py
    Se agregó la función calc_area_circulo

--- principal.py
+++ principal.py
@@ -2,6 +2,11 @@ def calc_area(lado):
 	area=lado*lado
 	print(f"El area es {area}")
 	
+def calc_area_circulo(radio):
+	area=lado*lado
+	print(f"El área es {area}")
+	
 	
 calc_area(2)
+calc_area_circulo(3)
 	
